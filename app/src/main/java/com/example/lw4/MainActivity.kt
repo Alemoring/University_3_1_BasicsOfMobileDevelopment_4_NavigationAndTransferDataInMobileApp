1f805339ac19e442b5578b3965cc8fae10a47c79
Alemoring <morgunov2004@list.ru>
2024-11-01 10:54:42 +0800

    initial commit

--- app/src/main/java/com/example/lw4/MainActivity.kt
+++ app/src/main/java/com/example/lw4/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
